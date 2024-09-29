8a2ce05307b233ef7619c3626e83dce8e0c84cde
Colin Le <Colin.Le@ucf.edu>
2024-09-28 20:57:54 -0400

    Add include directory

---
+++ include/test.hpp
@@ -0,0 +1,9 @@
+#ifndef TEST_HPP
+#define TEST_HPP
+
+class Test {
+public:
+    void sayHello();
+};
+
+#endif // TEST_HPP
---
+++ src/test.cpp
@@ -0,0 +1,6 @@
+#include "test.hpp"
+#include <iostream>
+
+void Test::sayHello() {
+    std::cout << "Hello from Test!" << std::endl;
+}
